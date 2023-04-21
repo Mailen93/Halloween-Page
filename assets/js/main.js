47aa80d4b0004634c8959a9c29b703b348f6fb0f
Mailen93 <m.labarrere2@gmail.com>
2023-04-21 19:29:04 -0300

    Second commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const navMenu = document.getElementById("nav-menu"),
+navToogle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close');
+
+if(navToogle){
+    navToogle.addEventListener("click", ()=>{
+        navMenu.classList.add("")
+    } )
+}
