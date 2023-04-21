099be13bcb77ee4ed7c147e24dff4f2abde16669
Mailen93 <m.labarrere2@gmail.com>
2023-04-21 20:29:16 -0300

    animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,35 @@ navClose=document.getElementById('nav-close');
 
 if(navToogle){
     navToogle.addEventListener("click", ()=>{
-        navMenu.classList.add("")
+        navMenu.classList.add("show-menu")
     } )
 }
+
+if(navClose){
+    navClose.addEventListener("click", ()=>{
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+//SCROLL REVEAL//
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+});
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+sr.reveal(`.category__data, .trick__content, .footer__content`,{
+    interval: 100
+})
+
+sr.reveal(`.about__data, .discount__img`,{
+    origin: "left"
+})
+
+sr.reveal(`.about__img, .discount__data`,{
+    origin: "right"
+})
